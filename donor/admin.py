1ff36803070124ad3f4c8bddc438f1e25fc2e803
Alexander Cohen <alex@capitolmuckraker.com>
2019-05-08 14:44:35 -0400

    rename donor model admin name to CNN

--- donor/admin.py
+++ donor/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from donor.models import *
 
 class DonorAdmin(admin.ModelAdmin):
-    search_fields = ['nyt_name']
+    search_fields = ['cnn_name']
 
 admin.site.register(Donor, DonorAdmin)

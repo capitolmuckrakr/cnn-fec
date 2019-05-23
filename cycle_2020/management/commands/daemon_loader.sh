5c5257a1ab23177c3ac42701c44b4e6cc7f4f998
capitolmuckrakr <alex@capitolmuckraker.com>
2019-05-23 02:49:52 +0000

    write sdo and sde to file list

--- cycle_2020/management/commands/daemon_loader.sh
+++ cycle_2020/management/commands/daemon_loader.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#https://ss64.com/bash/nohup.html
 logdir=~/launch
 mkdir -p ${logdir}
 logfilename="${2#*/}_$(date +%F_%H:%M:%S,%N)"
 echo $logfilename
 echo "== LAUNCH $@ ==" > ~/launch/${logfilename}_stdout.log
 echo "== LAUNCH $@ ==" > ~/launch/${logfilename}_stderr.log
-nohup "$@" >>${logdir}/${logfilename}_stdout.log 2>>${logdir}/${logfilename}_stderr.log &
+#nohup "$@" | tee -a -i ${logdir}/${logfilename}_stdout.log 2>>${logdir}/${logfilename}_stderr.log &
+nohup "$@" 1> >(tee -a -i ${logdir}/${logfilename}_stdout.log) 2> >(tee -a -i ${logdir}/${logfilename}_stderr.log) &

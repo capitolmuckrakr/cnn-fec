8708da200393e07109068997924bb017e1c639a6
capitolmuckrakr <alex@capitolmuckraker.com>
2019-05-28 21:27:52 +0000

    import and add summary_csv and cycle_summary_csv routers

--- cycle_2020/urls.py
+++ cycle_2020/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 from rest_framework import routers, serializers, viewsets
 from cycle_2020 import views
-from cycle_2020.cnn_views import summary, cycle_summary
+from cycle_2020.cnn_views import summary, summary_csv, cycle_summary, cycle_summary_csv
 from cycle_2020.api import *
 
 app_name = 'cycle_2020'
@@ -17,7 +17,9 @@ urlpatterns = API + [
         path('', views.index, name='index'),
         re_path(r'filings/$', views.filings, name='filings'), #we might someday want a real index but good enough for now
         re_path(r'cycle_summary/$', cycle_summary, name='cycle_summary'),
+        re_path(r'cycle_summary_csv/$', cycle_summary_csv, name='cycle_summary_csv'),
         re_path(r'summary/$', summary, name='summary'),
+        re_path(r'summary_csv/$', summary_csv, name='summary_csv'),
         re_path(r'contributions/$', views.contributions, name='contributions'),
         re_path(r'contributions_csv/$', views.contributions_csv, name='contributions_csv'),
         re_path(r'expenditures/$', views.expenditures, name='expenditures'),

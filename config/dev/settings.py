66cffdb73737ecc6b391119fc75509ae91df4cf6
capitolmuckrakr <alex@capitolmuckraker.com>
2019-05-28 14:01:10 +0000

    add django_extensions to apps in settings

--- config/dev/settings.py
+++ config/dev/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'django.contrib.humanize',
+    'django_extensions',
     'rest_framework',
     'pure_pagination',
     'storages',

0f04699488c8f0413333f410db8374520fe3c96a
capitolmuckrakr <alex@capitolmuckraker.com>
2019-05-29 07:43:11 +0000

    add bool to filter by cand watch list in summary and cycle_summary_views, remove redundant queries from main view. BUG: Conditional logic fails to catch invalid sort fields in passed params

--- cycle_2020/cnn_views.py
+++ cycle_2020/cnn_views.py
@@ -32,6 +32,7 @@ def get_summary_results(request):
     min_raised = request.GET.get('min_raised')
     min_date = request.GET.get('min_date')
     max_date = request.GET.get('max_date')
+    cnn_committees = request.GET.get('cnn_committees')
     
     results = Filing.objects.filter(active=True)
     if comm:
@@ -44,9 +45,15 @@ def get_summary_results(request):
         results = results.filter(date_signed__gte=min_date)
     if max_date:
         results = results.filter(date_signed__lte=max_date)
+    if cnn_committees:
+        cand_filer_ids= Candidate.objects.all().values('fec_committee_id')
+        results = results.filter(filer_id__in=cand_filer_ids)
 
     order_by = request.GET.get('order_by', 'filing_id')
     order_direction = request.GET.get('order_direction', 'DESC')
+    if order_by.startswith('cycle'):
+        order_by = 'filing_id'
+        order_direction = 'DESC'
     if order_by == 'period_disbursements_div_receipts':
         try:
             results = results.annotate(
@@ -110,6 +117,7 @@ def get_cycle_summary_results(request):
     min_raised = request.GET.get('min_raised')
     min_date = request.GET.get('min_date')
     max_date = request.GET.get('max_date')
+    cnn_committees = request.GET.get('cnn_committees')
     
     results = Filing.objects.filter(active=True,period_total_contributions__gt=0)
     if comm:
@@ -122,9 +130,15 @@ def get_cycle_summary_results(request):
         results = results.filter(date_signed__gte=min_date)
     if max_date:
         results = results.filter(date_signed__lte=max_date)
+    if cnn_committees:
+        cand_filer_ids= Candidate.objects.all().values('fec_committee_id')
+        results = results.filter(filer_id__in=cand_filer_ids)
 
     order_by = request.GET.get('order_by', 'filing_id')
     order_direction = request.GET.get('order_direction', 'DESC')
+    if order_by.startswith('period'):
+        order_by = 'filing_id'
+        order_direction = 'DESC'
     if order_by == 'cycle_disbursements_div_receipts':
         try:
             results = results.annotate(
@@ -182,150 +196,3 @@ def cycle_summary_csv(request):
     response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
     return response
 
-def get_contribution_results(request):
-    comm = request.GET.get('committee')
-    filing_id = request.GET.get('filing_id')
-    donor = request.GET.get('donor')
-    employer = request.GET.get('employer')
-    address = request.GET.get('address')
-    include_memo = request.GET.get('include_memo')
-    min_date = request.GET.get('min_date')
-    max_date = request.GET.get('max_date')
-    form_type = request.GET.get('form_type')
-
-    results = ScheduleA.objects.filter(active=True)
-    if not include_memo:
-        results = results.exclude(status='MEMO')
-    if filing_id:
-        results = results.filter(filing_id=filing_id)
-    if employer:
-        query = SearchQuery(employer)
-        results = results.filter(occupation_search=query)
-    if donor:
-        query = SearchQuery(donor)
-        results = results.filter(name_search=query)
-    if address:
-        query = SearchQuery(address)
-        results = results.filter(address_search=query)
-    if min_date:
-        results = results.filter(contribution_date__gte=min_date)
-    if max_date:
-        results = results.filter(contribution_date__lte=max_date)
-    if form_type:
-        results = results.filter(form_type=form_type)
-
-    if comm:
-        matching_committees = Committee.find_committee_by_name(comm)
-        comm_ids = [c.fec_id for c in matching_committees]
-        results = results.filter(filer_committee_id_number__in=comm_ids)
-
-    order_by = request.GET.get('order_by', 'contribution_amount')
-    order_direction = request.GET.get('order_direction', 'DESC')
-    if order_direction == "DESC":
-        results = results.order_by('-{}'.format(order_by))
-    else:
-        results = results.order_by(order_by)
-    return results
-
-def contributions(request):
-    form = ContributionForm(request.GET)
-    if not request.GET:
-        return render(request, '2020/contributions.html', {'form': form,  'contact':settings.CONTACT})
-
-    results = get_contribution_results(request)
-
-    results_sum = None if request.GET.get('include_memo') else results.aggregate(Sum('contribution_amount'))
-
-    csv_url = reverse('2020:contributions_csv') + "?"+ request.GET.urlencode()
-
-    paginator = Paginator(results, 50)
-    page = request.GET.get('page')
-    results = paginator.get_page(page)
-
-    
-    return render(request, '2020/contributions.html', {'form': form, 'results':results, 'results_sum':results_sum, 'csv_url':csv_url, 'contact':settings.CONTACT})
-
-def contributions_csv(request):
-    results = get_contribution_results(request)
-    filename = "ScheduleA_{}.csv".format(time.strftime("%Y%m%d-%H%M%S"))
-
-    def rows():
-        yield ScheduleA.export_fields()
-        for result in results:
-            yield result.csv_row()
-
-    pseudo_buffer = Echo()
-    writer = csv.writer(pseudo_buffer)
-    response = StreamingHttpResponse((writer.writerow(row) for row in rows()),
-                                     content_type="text/csv")
-    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
-    return response
-
-def presidential_csv(request):
-    deadline = request.GET.get('deadline')
-
-    results = Candidate.objects.filter(office='P').order_by('party', 'name')
-    filename = "Candidates_{}.csv".format(time.strftime("%Y%m%d-%H%M%S"))
-
-    candidate_fields = ['name', 'office', 'party', 'incumbent']
-    filing_fields = ['filing_id','form','filer_id',
-            'committee_name', 'cash_on_hand_close_of_period', 'is_amendment',
-            'period_total_receipts','cycle_total_receipts',
-            'period_total_disbursements', 'cycle_total_disbursements',
-            'period_individuals_unitemized', 'cycle_individuals_unitemized',
-            'period_individuals_itemized', 'cycle_individuals_itemized',
-            'period_transfers_from_authorized','cycle_transfers_from_authorized',
-            'period_individual_contribution_total', 'cycle_individual_contribution_total',
-            'coverage_from_date', 'coverage_through_date']
-
-    def rows_with_totals():
-        yield candidate_fields+filing_fields+['cycle_candidate_donations_plus_loans','period_candidate_donations_plus_loans']
-        for result in results:
-            if deadline:
-                filing = result.filing_by_deadline(deadline)
-            else:
-                filing = result.most_recent_filing()
-
-            row = []
-            for f in candidate_fields:
-                value = getattr(result, f)
-                if value is None:
-                    row.append("")
-                else:
-                    row.append(value)
-            for f in filing_fields:
-                if not filing:
-                    if f == 'filer_id':
-                        #if there is no filing, use the candidate's committee id
-                        value = getattr(result, "fec_committee_id")
-                        if value is not None:
-                            row.append(value)
-                            continue
-                    row.append("")
-                    continue
-                value = getattr(filing, f)
-                if value is None:
-                    row.append("")
-                else:
-                    row.append(value)
-            if filing:
-                cycle_candidate_donations_plus_loans = filing.cycle_candidate_donations_plus_loans #this has to be done separately bc it's a property.
-                period_candidate_donations_plus_loans = filing.period_candidate_donations_plus_loans
-            if not filing or cycle_candidate_donations_plus_loans is None:
-                row.append("")
-            else:
-                row.append(cycle_candidate_donations_plus_loans)
-            if not filing or period_candidate_donations_plus_loans is None:
-                row.append("")
-            else:
-                row.append(period_candidate_donations_plus_loans)
-            yield row
-
-
-    pseudo_buffer = Echo()
-    writer = csv.writer(pseudo_buffer)
-    response = StreamingHttpResponse((writer.writerow(row) for row in rows_with_totals()),
-                                     content_type="text/csv")
-
-    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
-    return response

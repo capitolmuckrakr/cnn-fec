2b0b6248c3bf06e049f04634a4930f1a5c5a14b3
capitolmuckrakr <alex@capitolmuckraker.com>
2019-06-05 20:51:11 +0000

    add ordering, list_display to DonorAdmin

--- donor/admin.py
+++ donor/admin.py
@@ -3,5 +3,7 @@ from donor.models import *
 
 class DonorAdmin(admin.ModelAdmin):
     search_fields = ['cnn_name']
+    ordering = ('cnn_name',)
+    list_display = ('cnn_name','contribution_total_2020')
 
 admin.site.register(Donor, DonorAdmin)

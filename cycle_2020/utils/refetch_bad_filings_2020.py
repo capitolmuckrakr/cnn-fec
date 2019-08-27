ddc698ae2b3f200317bb6c48ff13b9c4976d392c
capitolmuckrakr <alex@capitolmuckraker.com>
2019-08-27 19:52:09 +0000

    massive refactoring

--- cycle_2020/utils/refetch_bad_filings_2020.py
+++ cycle_2020/utils/refetch_bad_filings_2020.py
@@ -1,62 +1,90 @@
-# coding: utf-8
-from cycle_2020.utils import loader
 import os, logging
+from cycle_2020.utils.loader import download_filings
+from cycle_2020.models import FilingStatus
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=LOGLEVEL)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.WARNING)
-stream_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-stream_handler.setFormatter(stream_format)
+
 filing_dir = os.environ.get('HOME') + '/scripts/cnn-fec/filings/'
 
-def bad_file_check(file,  filing_dir=filing_dir):
-    #pop open a downloaded file and flag it if it's not a readable csv
+def readable_file_check(file, filing_dir=filing_dir):
+    """pop open a downloaded file and flag it if it's not a readable csv.
+
+        Args:
+            file (str): A numbered FEC csv file, for example '123456.csv'.
+            filing_dir (str, optional): An absolute directory path for the file, defaults
+                to a 'filings' directory under the project root.
+
+        Returns:
+            True if the filing can be opened and read and doesn't appear to be a webpage,
+                False otherwise. Logs details of False results if LOGLEVEL has been set to 'info'.
+    """
     filename = '{}{}'.format(filing_dir, file)
     #with open(filename, errors="backslashreplace") as f:
     with open(filename) as f:
         try:
             if f.readlines(1)[0] == '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"\n':
+                logger.info("File {} isn't a csv, FEC site is returning empty web pages".format(file))
                 return False
             else:
                 return True
         except UnicodeDecodeError:
-            logger.warning("File {} can't be decoded".format(file))
+            logger.info("File {} can't be decoded".format(file))
             return False
         except IndexError:
-            logger.warning("File {} can't be indexed".format(file))
-            return False
+            if file == '.placeholder':
+                logger.debug("Skipping {}, not a filing".format(file))
+                return True
+            else:
+                logger.info("File {} can't be indexed".format(file))
+                return False
     filename = None
 
-def recheck_existing_filings(filing_dir=filing_dir):
-    #find bad filings
-    existing_filings = os.listdir(filing_dir)
-    retry_filings = set()
-    filecounter = 0
-    for file in existing_filings:
-        filecounter += 1
-        if filecounter % 10000 == 0:
-            logger.info('{} of {} files, found {} bad files'.format(filecounter,len(existing_filings),len(retry_filings)))
-        if not bad_file_check(file, filing_dir=filing_dir):
-            retry_filings.add(file.split('.')[0])
-    return retry_filings
-
-def delete_bad_files(filings, filing_dir=filing_dir):
-    for filing_id in filings:
-        filename = '{}{}.csv'.format(filing_dir, filing_id)
-        if os.path.isfile(filename):
-            try:
-                os.unlink(filename)
-            except:
-                logger.error('Error deleting {}'.format(filename))
-                return False
-            try:
-                loader.create_or_update_filing_status(filing_id,'FAILED')
-            except:
-                logger.error('Error updating status of filing {}'.format(filing_id))
-                return False
+def delete_file(file, filing_dir=filing_dir):
+    filename = '{}{}.csv'.format(filing_dir, file)
+    if os.path.isfile(filename):
+        try:
+            os.unlink(filename)
+            logger.info('{}.csv deleted'.format(file))
+        except Exception as err:
+            logger.error('Error deleting {}'.format(filename))
+            raise err
     return True
-        
+
+def reset_refused_filing_to_failed(filing_id):
+    try:
+        fs = FilingStatus.objects.filter(filing_id=filing_id)
+        if len(fs) > 0:
+            fs = fs[0]
+            if fs.status == 'REFUSED':
+                fs.status = 'FAILED'
+                fs.save()
+                logger.warning('Filing {} status updated to FAILED'.format(filing_id))
+        return True
+    except Exception as err:
+        logger.error("Filing {} status can't be reset".format(file))
+        raise err
+
+def recheck_existing_files(filing_dir=filing_dir):
+    #find unreadable files and redownload them
+    try:
+        existing_files = os.listdir(filing_dir)
+        retry_filings = set()
+        filecounter = 0
+        for file in existing_files:
+            filecounter += 1
+            if filecounter % 10000 == 0:
+                logger.debug('{} of {} files, found {} unreadable files'.format(filecounter,len(existing_files),len(retry_filings)))
+            if not readable_file_check(file, filing_dir=filing_dir):
+                if reset_refused_filing_to_failed(filing_id):
+                    if delete_file(file, filing_dir=filing_dir):
+                        retry_filings.add(file.split('.')[0])
+        return retry_filings
+    except Exception as err:
+        logger.critical("File recheck failed!")
+        raise err
+
 if __name__ == '__main__':
-    bad_files = recheck_existing_filings(filing_dir)
-    delete_bad_files(bad_files, filing_dir)
-    loader.download_filings(bad_files, filing_dir)
+    unreadable_files = recheck_existing_files(filing_dir)
+    download_filings(unreadable_files, filing_dir)

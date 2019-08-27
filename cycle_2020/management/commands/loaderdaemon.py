58965c4656e87189804ca9f01083b0bbdec96b66
capitolmuckrakr <alex@capitolmuckraker.com>
2019-08-27 23:25:37 +0000

    modify log settings

--- cycle_2020/management/commands/loaderdaemon.py
+++ cycle_2020/management/commands/loaderdaemon.py
@@ -16,7 +16,9 @@ from django.core.management.base import BaseCommand, CommandError
 
 import logging
 
-logger = logging.getLogger('__name__')
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+daemonlogger = logging.getLogger("cnn-fec."+__name__)
+daemonlogger.setLevel(LOGLEVEL)
 
 class Command(BaseCommand):
 
@@ -28,29 +30,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         
-        LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
-        #logger = logging.getLogger('cnn-fec')
-        logger = logging.getLogger('__name__')
-        logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=LOGLEVEL)
-        level = getattr(logging,LOGLEVEL)
-        logger.setLevel(level)
-        
         fec_time=pytz.timezone('US/Eastern') #fec time is eastern
 
         unparsed_start = datetime.datetime.now(fec_time) - datetime.timedelta(days=2)
         start_date = unparsed_start.strftime('%Y%m%d')
         unparsed_end = datetime.datetime.now(fec_time) + datetime.timedelta(days=1)
         end_date = unparsed_end.strftime('%Y%m%d')
-#        logname = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S,%f")
         if options['filing_dir']:
             filing_dir = options['filing_dir']
         else:
             filing_dir = 'filings/'
 
-        logger.info("looking for filings for period {}-{}".format(start_date, end_date))
+        daemonlogger.info("looking for filings for period {}-{}".format(start_date, end_date))
         filings = loader.get_filing_list(start_date, end_date)
         if not filings:
-            logger.warning("failed to find any filings for period {}-{}".format(start_date, end_date))
+            daemonlogger.warning("failed to find any filings for period {}-{}".format(start_date, end_date))
 
         
         #loader.download_filings(filings, filing_dir)

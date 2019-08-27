4962a461c3ab17b7317ff3f0f69ded91867e1498
capitolmuckrakr <alex@capitolmuckraker.com>
2019-08-27 23:24:48 +0000

    create management command to clean out unreadable files

---
+++ cycle_2020/management/commands/filecleaner.py
@@ -0,0 +1,46 @@
+import pytz
+import datetime
+import os
+import requests
+import csv
+import process_filing
+import time
+import traceback
+import sys
+
+from cycle_2020.utils import unreadable_files_2020
+
+from cycle_2020.utils import loader
+
+from django.core.management.base import BaseCommand, CommandError
+
+import logging
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+cleanerlogger = logging.getLogger("cnn-fec."+__name__)
+cleanerlogger.setLevel(LOGLEVEL)
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--filing_dir',
+            dest='filing_dir',
+            help='where to save and read filings from')
+    #default is to do 
+
+    def handle(self, *args, **options):
+        
+        if options['filing_dir']:
+            filing_dir = options['filing_dir']
+        else:
+            filing_dir = os.environ.get('HOME') + '/scripts/cnn-fec/filings/'
+
+        filings = unreadable_files_2020.recheck_existing_files(filing_dir)
+        if  len(filings) >0:
+            cleanerlogger.warning("Found and deleted {} unreadable files".format(len(filings)))
+
+        
+        #loader.download_filings(filings, filing_dir)
+        #loader.load_filings(filing_dir)
+
+

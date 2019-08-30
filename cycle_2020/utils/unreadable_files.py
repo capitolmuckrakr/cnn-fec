b4bac1fff96784ec08977198c1993c9c41793d05
capitolmuckrakr <alex@capitolmuckraker.com>
2019-08-30 19:16:49 -0400

    rename logger extra journal dict, add to remaining function calls

--- cycle_2020/utils/unreadable_files.py
+++ cycle_2020/utils/unreadable_files.py
@@ -1,5 +1,4 @@
 import os, sys
-#sys.path.insert(0,sys.path[6])
 import logging
 from cycle_2020.models import FilingStatus
 
@@ -23,41 +22,51 @@ def readable_file_check(file, filing_dir=filing_dir, myextra=None):
     """
     filename = '{}{}'.format(filing_dir, file)
     if myextra:
-        x=myextra.copy()
-        x['FILING']=file.split('.')[0]
+        myextra=myextra.copy()
+        myextra['FILING']=file.split('.')[0]
     else:
-        x=''
+        myextra=''
     #with open(filename, errors="backslashreplace") as f:
     with open(filename) as f:
         try:
             if f.readlines(1)[0] == '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"\n':
-                logger.info("File {} isn't a csv, FEC site returned an empty web page instead".format(file),extra=x)
+                logger.info("File {} isn't a csv, FEC site returned an empty web page instead".format(file),extra=myextra)
                 return False
             else:
                 return True
         except UnicodeDecodeError:
-            logger.info("File {} can't be decoded".format(file),extra=x)
+            logger.info("File {} can't be decoded".format(file),extra=myextra)
             return False
         except IndexError:
             if file == '.placeholder':
-                logger.debug("Skipping {}, not a filing".format(file),extra=x)
+                logger.debug("Skipping {}, not a filing".format(file),extra=myextra)
                 return True
             else:
-                logger.info("File {} can't be indexed".format(file),extra=x)
+                logger.info("File {} can't be indexed".format(file),extra=myextra)
                 return False
 
 def delete_file(file, filing_dir=filing_dir, myextra=None):
     filename = '{}{}.csv'.format(filing_dir, file)
+    if myextra:
+        myextra=myextra.copy()
+        myextra['FILING']=file.split('.')[0]
+    else:
+        myextra=''
     if os.path.isfile(filename):
         try:
             os.unlink(filename)
-            logger.info('{}.csv deleted'.format(file))
+            logger.info('{}.csv deleted'.format(file),extra=myextra)
         except Exception as err:
-            logger.error('Error deleting {}'.format(filename))
+            logger.error('Error deleting {}'.format(filename),extra=myextra)
             raise err
     return True
 
 def reset_refused_filing_to_failed(filing_id, myextra=None):
+    if myextra:
+        myextra=myextra.copy()
+        myextra['FILING']=filing_id
+    else:
+        myextra=''
     try:
         fs = FilingStatus.objects.filter(filing_id=filing_id)
         if len(fs) > 0:
@@ -65,10 +74,10 @@ def reset_refused_filing_to_failed(filing_id, myextra=None):
             if fs.status == 'REFUSED':
                 fs.status = 'FAILED'
                 fs.save()
-                logger.warning('Filing {} status updated to FAILED'.format(filing_id))
+                logger.info('Filing {} status updated to FAILED'.format(filing_id),extra=myextra)
         return True
     except Exception as err:
-        logger.error("Filing {} status can't be reset".format(file))
+        logger.error("Filing {} status can't be reset".format(file),extra=myextra)
         raise err
 
 def recheck_existing_files(filing_dir=filing_dir, myextra=None):

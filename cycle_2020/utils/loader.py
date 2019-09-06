f5a0e8c36032b4f939fd2d745cdcc47c54b309bd
capitolmuckrakr <alex@capitolmuckraker.com>
2019-09-06 13:29:24 -0400

    Fix tag in loader.load_filings exception handler myextra dict

--- cycle_2020/utils/loader.py
+++ cycle_2020/utils/loader.py
@@ -778,7 +778,7 @@ def load_filings(filing_dir, myextra=None):
             logging.log(title="Bad FEC filename",
                     text='did not recognize filing {}'.format(filename),
                     tags=["cnn-fec", "result:warn"])
-            myextra['TAGS']="cnn-fec", "result:warn"
+            myextra['TAGS']="cnn-fec, result:warn"
             logger.warn('Bad FEC filename {}'.format(filename),extra=myextra)
             continue
 

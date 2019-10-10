7c2af4c5c7bb8d9ba936b4224587be68874fba27
capitolmuckrakr <alex@capitolmuckraker.com>
2019-10-10 19:23:58 -0400

    substitute instance public endpoint for placeholder in nginx/sites-available

---
+++ utils/ipfix.py
@@ -0,0 +1,10 @@
+from fileinput import FileInput
+
+with FileInput(files=['/etc/nginx/sites-available/cnn-fec'], inplace=True) as oldfile:
+    for line in oldfile:
+        if "EC2IP" in line:
+            myip=get_ipython().getoutput('curl -s -o- http://169.254.169.254/latest/meta-data/public-hostname/')
+            myip="    server_name "+myip[0]
+            print(myip)
+        else:
+            print(line,end='')

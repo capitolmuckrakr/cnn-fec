1eb32015215c13fa723439dcf1f77d5e5d70f226
capitolmuckrakr <alex@capitolmuckraker.com>
2019-10-13 23:26:03 -0400

    fix nginx server_name

--- utils/ipfix.py
+++ utils/ipfix.py
@@ -4,7 +4,7 @@ with FileInput(files=['/home/ubuntu/scripts/cnn-fec/systemd/cnn-fec'], inplace=T
     for line in oldfile:
         if "EC2IP" in line:
             myip=get_ipython().getoutput('curl -s -o- http://169.254.169.254/latest/meta-data/public-hostname/')
-            myip="    server_name "+myip[0]
+            myip="    server_name "+myip[0] +';'
             print(myip)
         else:
             print(line,end='')
